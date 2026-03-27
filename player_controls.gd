4503e75297009d3b8366ea4056713c1517c541ed
reagan <reaganwa@usc.edu>
2026-03-27 14:17:06 -0700

    Paddles are performing okay

--- player_controls.gd
+++ player_controls.gd
@@ -9,11 +9,20 @@ const rotationAngle : float = -PI / 4
 var left_bumper_default_rotation : float
 var right_bumper_default_rotation : float
 
+func _ready() -> void:
+	left_bumper_default_rotation = LeftPaddle.rotation
+	right_bumper_default_rotation = RightPaddle.rotation
+
 func _process(delta: float) -> void:
 	if (Input.is_action_pressed("LeftBumper")):
 		var nextRotationAngle = lerp(LeftPaddle.rotation, rotationAngle, delta * rotationSpeed)
 		LeftPaddle.rotation = nextRotationAngle
 	else:
-		var nextRotationAngle = lerp(LeftPaddle.rotation, 0.0, delta * rotationSpeed)
+		var nextRotationAngle = lerp(LeftPaddle.rotation, left_bumper_default_rotation, delta * rotationSpeed)
 		LeftPaddle.rotation = nextRotationAngle
-	#if (Input.is_action_pressed("RightBumper")):
+	if (Input.is_action_pressed("RightBumper")):
+		var nextRotationAngle = lerp(RightPaddle.rotation, -rotationAngle, delta * rotationSpeed)
+		RightPaddle.rotation = nextRotationAngle
+	else:
+		var nextRotationAngle = lerp(RightPaddle.rotation, right_bumper_default_rotation, delta * rotationSpeed)
+		RightPaddle.rotation = nextRotationAngle
